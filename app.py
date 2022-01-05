894c068cc4ff687ebd29419619bf474f9b7e2e4b
YENOGGY <57917541+Yenoggy@users.noreply.github.com>
2022-01-05 23:41:45 +0300

    Привет от Flask

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from flask_migrate import Migrate
 from markupsafe import escape
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
 db.app = app
 db.init_app(app)
 migrate = Migrate(app, db)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html', title='Задачи по физике')
+def main():
+    tasks = Task.query.all()
+    return render_template('index.html', title='Задачи по физике', tasks=tasks)
 
 
 if __name__ == '__main__':
