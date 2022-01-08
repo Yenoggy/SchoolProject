68aaf6ad6d79edda8168ef0b8119f614d216cb61
YENOGGY <57917541+Yenoggy@users.noreply.github.com>
2022-01-08 23:56:30 +0300

    Привет от Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from models import db, Task
 from flask_migrate import Migrate
 from markupsafe import escape
