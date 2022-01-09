eaa1ce437456cc20305767f2a8e260750d4f48ed
YENOGGY <57917541+Yenoggy@users.noreply.github.com>
2022-01-09 20:55:57 +0300

    Fix Bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 from models import db, Task
 from flask_migrate import Migrate
 from markupsafe import escape
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
 db.app = app
